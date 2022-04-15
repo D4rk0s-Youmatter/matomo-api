8f52ae09d6da708568b5a6fc82ed0ecb4fc67235
Grégory COLLIN <gregory@d4rk0s.net>
2022-04-15 10:23:33 +0200

    wip

--- src/Command/GetMostViewedPages.php
+++ src/Command/GetMostViewedPages.php
@@ -14,12 +14,6 @@ class GetMostViewedPages
     {
         global $wpdb;
 
-        // Il faut boucler sur l'ensemble des articles et récupérer le post-name
-        // Ca sera la clef à faire matcher avec la colonne label du retour de matomo.
-        // Il faut vérifier aussi si il nous faut la même chose.
-        // Une fois que l'on a pris les 8 premiers,
-
-        // https://matomo.youmatter.world/?module=API&filter_limit=50&idSubtable=336&showColumns=nb_visits&expanded=0&method=Actions.getPageUrls&idSite=1&period=year&date=yesterday&format=JSON&token_auth=6746d778730db1d260ca40fb6250a9bb&filter_sort_column=nb_visits&filter_sort_order=desc
         $postData = [
             'filter_limit' => 15,
             'idSubtable' => 336,
